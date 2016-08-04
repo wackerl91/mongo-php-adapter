fb09607cfdf0913a11d47781c740ce8a9670b0df
Andreas Braun <git@alcaeus.org>
2016-08-04 09:13:01 +0200

    Build hhvm extension on travis

---
+++ .travis/hhvm-mongodb.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+HHVM_VERSION=$1
+
+mkdir -p ~/build/hhvm-mongodb
+cd ~/build/hhvm-mongodb
+wget https://github.com/mongodb/mongo-hhvm-driver/releases/download/{$HHVM_VERSION}/hhvm-mongodb-{$HHVM_VERSION}.tgz
+tar -xvzf hhvm-mongodb-{$HHVM_VERSION}.tgz
+cd hhvm-mongodb-{$HHVM_VERSION}
+hphpize
+cmake .
+make configlib
+make install
+
+echo "hhvm.dynamic_extensions[mongodb]=mongodb.so" >> /etc/hhvm/php.ini

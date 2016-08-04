866cd825b3cede4d115a2af03325ad296229ffd9
Andreas Braun <git@alcaeus.org>
2016-08-04 09:58:58 +0200

    Fix curly brackets. SMH.

--- .travis/hhvm-mongodb.sh
+++ .travis/hhvm-mongodb.sh
@@ -4,9 +4,9 @@ HHVM_VERSION=$1
 
 mkdir -p ~/build/hhvm-mongodb
 cd ~/build/hhvm-mongodb
-wget https://github.com/mongodb/mongo-hhvm-driver/releases/download/{$HHVM_VERSION}/hhvm-mongodb-{$HHVM_VERSION}.tgz
-tar -xvzf hhvm-mongodb-{$HHVM_VERSION}.tgz
-cd hhvm-mongodb-{$HHVM_VERSION}
+wget https://github.com/mongodb/mongo-hhvm-driver/releases/download/${HHVM_VERSION}/hhvm-mongodb-${HHVM_VERSION}.tgz
+tar -xvzf hhvm-mongodb-${HHVM_VERSION}.tgz
+cd hhvm-mongodb-${HHVM_VERSION}
 hphpize
 cmake .
 make configlib
